53d533a130058b3245c7e49a45e8fcb2b5f0839e
hmd582 <hammadmunir582@gmail.com>
2025-02-17 15:23:43 +0500

    Add files via upload

---
+++ currencyConversion.js
@@ -0,0 +1,244 @@
+const countryList = {
+    AED: "AE",
+    AFN: "AF",
+    XCD: "AG",
+    ALL: "AL",
+    AMD: "AM",
+    ANG: "AN",
+    AOA: "AO",
+    AQD: "AQ",
+    ARS: "AR",
+    AUD: "AU",
+    AZN: "AZ",
+    BAM: "BA",
+    BBD: "BB",
+    BDT: "BD",
+    XOF: "BE",
+    BGN: "BG",
+    BHD: "BH",
+    BIF: "BI",
+    BMD: "BM",
+    BND: "BN",
+    BOB: "BO",
+    BRL: "BR",
+    BSD: "BS",
+    NOK: "BV",
+    BWP: "BW",
+    BYR: "BY",
+    BZD: "BZ",
+    CAD: "CA",
+    CDF: "CD",
+    XAF: "CF",
+    CHF: "CH",
+    CLP: "CL",
+    CNY: "CN",
+    COP: "CO",
+    CRC: "CR",
+    CUP: "CU",
+    CVE: "CV",
+    CYP: "CY",
+    CZK: "CZ",
+    DJF: "DJ",
+    DKK: "DK",
+    DOP: "DO",
+    DZD: "DZ",
+    ECS: "EC",
+    EEK: "EE",
+    EGP: "EG",
+    ETB: "ET",
+    EUR: "FR",
+    FJD: "FJ",
+    FKP: "FK",
+    GBP: "GB",
+    GEL: "GE",
+    GGP: "GG",
+    GHS: "GH",
+    GIP: "GI",
+    GMD: "GM",
+    GNF: "GN",
+    GTQ: "GT",
+    GYD: "GY",
+    HKD: "HK",
+    HNL: "HN",
+    HRK: "HR",
+    HTG: "HT",
+    HUF: "HU",
+    IDR: "ID",
+    ILS: "IL",
+    INR: "IN",
+    IQD: "IQ",
+    IRR: "IR",
+    ISK: "IS",
+    JMD: "JM",
+    JOD: "JO",
+    JPY: "JP",
+    KES: "KE",
+    KGS: "KG",
+    KHR: "KH",
+    KMF: "KM",
+    KPW: "KP",
+    KRW: "KR",
+    KWD: "KW",
+    KYD: "KY",
+    KZT: "KZ",
+    LAK: "LA",
+    LBP: "LB",
+    LKR: "LK",
+    LRD: "LR",
+    LSL: "LS",
+    LTL: "LT",
+    LVL: "LV",
+    LYD: "LY",
+    MAD: "MA",
+    MDL: "MD",
+    MGA: "MG",
+    MKD: "MK",
+    MMK: "MM",
+    MNT: "MN",
+    MOP: "MO",
+    MRO: "MR",
+    MTL: "MT",
+    MUR: "MU",
+    MVR: "MV",
+    MWK: "MW",
+    MXN: "MX",
+    MYR: "MY",
+    MZN: "MZ",
+    NAD: "NA",
+    XPF: "NC",
+    NGN: "NG",
+    NIO: "NI",
+    NPR: "NP",
+    NZD: "NZ",
+    OMR: "OM",
+    PAB: "PA",
+    PEN: "PE",
+    PGK: "PG",
+    PHP: "PH",
+    PKR: "PK",
+    PLN: "PL",
+    PYG: "PY",
+    QAR: "QA",
+    RON: "RO",
+    RSD: "RS",
+    RUB: "RU",
+    RWF: "RW",
+    SAR: "SA",
+    SBD: "SB",
+    SCR: "SC",
+    SDG: "SD",
+    SEK: "SE",
+    SGD: "SG",
+    SKK: "SK",
+    SLL: "SL",
+    SOS: "SO",
+    SRD: "SR",
+    STD: "ST",
+    SVC: "SV",
+    SYP: "SY",
+    SZL: "SZ",
+    THB: "TH",
+    TJS: "TJ",
+    TMT: "TM",
+    TND: "TN",
+    TOP: "TO",
+    TRY: "TR",
+    TTD: "TT",
+    TWD: "TW",
+    TZS: "TZ",
+    UAH: "UA",
+    UGX: "UG",
+    USD: "US",
+    UYU: "UY",
+    UZS: "UZ",
+    VEF: "VE",
+    VND: "VN",
+    VUV: "VU",
+    YER: "YE",
+    ZAR: "ZA",
+    ZMK: "ZM",
+    ZWD: "ZW",
+};
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector(".btn");
+
+// for adding list of countries in dropdown
+for (let select of dropdowns) {
+    for (let currCode in countryList) {
+        const newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        if (select.name === "from" && currCode === "USD") {
+            newOption.selected = "selected";
+        } else if (select.name === "to" && currCode === "PKR") {
+            newOption.selected = "selected";
+        }
+        select.append(newOption);
+    }
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+    });
+}
+
+const updateFlag = (element) => {
+        let currCode = element.value;
+        console.log(element.value);
+        let countryCode = countryList[currCode];
+        let newSrc = `https://flagsapi.com/${countryCode}/shiny/64.png`;  
+        let img = element.parentElement.querySelector("img");  
+        img.src = newSrc;    
+};
+
+const arrow = document.querySelector(".arrow i");
+
+const updateExchangeRate = async () => {
+    const fromCurrencyValue = document.querySelector(".from select").value;
+    const toCurrencyValue = document.querySelector(".to select").value;
+    const amount = parseFloat(document.querySelector(".amountInput").value);
+
+    if (isNaN(amount) || amount <= 0) {
+        document.querySelector(".msg p").innerHTML = "Please enter a valid amount.";
+        return;
+    }
+
+    try {
+        const response = await fetch(`https://api.exchangerate-api.com/v4/latest/${fromCurrencyValue}`);
+        const data = await response.json();
+        const rate = data.rates[toCurrencyValue];
+        const totalAmount = (amount * rate).toFixed(2);
+
+        document.querySelector(".msg p").innerHTML = `${amount} ${fromCurrencyValue} = ${totalAmount} ${toCurrencyValue}`;
+    } catch (error) {
+        document.querySelector(".msg p").innerHTML = "Error fetching exchange rate. Please try again.";
+    }
+};
+
+arrow.addEventListener('click', () => {
+    const fromSelect = document.querySelector(".from select");
+    const toSelect = document.querySelector(".to select");
+
+    // Swap the selected values
+    const tempValue = fromSelect.value;
+    fromSelect.value = toSelect.value;
+    toSelect.value = tempValue;
+
+    // Update the flags
+    updateFlag(fromSelect);
+    updateFlag(toSelect);
+
+    // Update the exchange rate
+    updateExchangeRate();
+});
+
+btn.addEventListener('click', updateExchangeRate);
+
+// async function getExchangeRate(fromCurrency, toCurrency) {
+//     const response = await fetch(`https://api.exchangerate-api.com/v4/latest/${fromCurrency}`);
+//     const data = await response.json();
+//     const rate = data.rates[toCurrency];    
+//     console.log(rate);
+// }
+
+
+
